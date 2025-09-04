23b1bbc37515b8a95b6bed5fd3614a572a644c9c
kanikatiwari06 <kanikatiwari2005@gmail.com>
2025-09-04 07:29:20 +0530

    add routing and query parameter handling to HTTP server

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const http= require("http");
 const fs= require("fs");
+const url= require("url");
 
 const myServer= http.createServer((req, res)=>{
-    const log= `${Date.now()}: New Req Received\n`;
+    if(req.url === "/favicon.ico") return res.end();
+    const log= `${Date.now()}: ${req.url} New Req Received\n`;
+    const myUrl= url.parse(req.url,true);
+    console.log(myUrl);
     fs.appendFile("log.txt",log,(err,data)=>{
-        switch(req.ur){
-            case '/': res.end("HomePage");
+        switch(myUrl.pathname){
+            case '/':
+             res.end("HomePage");
             break;
-            case'/about':res.end("This is a http server");
+            case'/about':
+            const username= myUrl.query.myname
+            res.end(`Hi, ${username}`);
+            break;
+            case"/search":
+            const search= myUrl.query.search_query;
+            res.end("here are your results for"+ search);
             break;
             default:
                 res.end("404 not found")
