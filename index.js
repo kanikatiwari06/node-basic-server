a082251188befad6c5b878e86990dd892fb45a28
kanikatiwari06 <kanikatiwari2005@gmail.com>
2025-09-05 22:20:29 +0530

    add basic routing with query parameters and logging

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
-const http= require("http");
-const fs= require("fs");
-const url= require("url");
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
 
-const myServer= http.createServer((req, res)=>{
-    if(req.url === "/favicon.ico") return res.end();
-    const log= `${Date.now()}: ${req.url} New Req Received\n`;
-    const myUrl= url.parse(req.url,true);
-    console.log(myUrl);
-    fs.appendFile("log.txt",log,(err,data)=>{
-        switch(myUrl.pathname){
-            case '/':
-             res.end("HomePage");
+const myServer = http.createServer((req, res) => {
+    if (req.url === "/favicon.ico") return res.end();
+
+    const myUrl = url.parse(req.url, true);
+
+    const log = `${new Date().toISOString()} [${req.method}] ${myUrl.pathname}\n`;
+
+    fs.appendFile("log.txt", log, (err) => {
+        if (err) console.error("Error writing log:", err);
+    });
+
+    switch (myUrl.pathname) {
+        case "/":
+            res.writeHead(200, { "Content-Type": "text/plain" });
+            res.end("Home Page");
             break;
-            case'/about':
-            const username= myUrl.query.myname
+
+        case "/about":
+            const username = myUrl.query.myname || "Guest";
+            res.writeHead(200, { "Content-Type": "text/plain" });
             res.end(`Hi, ${username}`);
             break;
-            case"/search":
-            const search= myUrl.query.search_query;
-            res.end("here are your results for"+ search);
+
+        case "/search":
+            const search = myUrl.query.search_query || "nothing";
+            res.writeHead(200, { "Content-Type": "text/plain" });
+            res.end(`Here are your results for: ${search}`);
             break;
-            default:
-                res.end("404 not found")
-        }
-    })
-})
-myServer.listen(8000,()=> console.log("server started"))
+
+        default:
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.end("404 Not Found");
+    }
+});
+
+myServer.listen(8000, () => console.log("🚀 Server started on port 8000"));
